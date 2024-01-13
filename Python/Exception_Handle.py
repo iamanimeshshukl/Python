25ac0d435b0a893c340dc461044ec741f56fb0bd
Animesh shukla <75159346+iamanimeshshukl@users.noreply.github.com>
2024-01-13 18:56:44 +0530

    Update Exception_Handle.py

--- Python/Exception_Handle.py
+++ Python/Exception_Handle.py
@@ -1,10 +1,31 @@
 #Exception Handling in Python
 
-
 try:
     x = int(input("Enter your age-"))
     print("Your Age is-",x)
 except Exception as e:
     print("Something is wrong",e)
 
+
+
+#Use of Assert keyword 
+a = 4
+b = 0
+
+assert b!=0 
+print(a/b)
+
+
+#use of raise keyword
+
+def divide(a,b):
+   if b == 0:
+      raise ValueError("CANNOT DIVIDE ZERO")
+   return a/b 
+
+try:
+   result = divide(10,2)
+   print(result)
+except ValueError as e:
+   print(e)
 
